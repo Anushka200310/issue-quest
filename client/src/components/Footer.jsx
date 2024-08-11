cea1c4c6effdd0155d898af1a2b3984963ad1b79
Anushka <banerjeeanushka154@gmail.com>
2024-08-11 16:54:51 +0530

    commit

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
-    <footer className="px-4 py-8 mt-2 relative dark:bg-gray-800 dark:text-gray-200">
+    <footer className="relative px-4 py-8 mt-2 dark:bg-gray-800 dark:text-gray-200">
 	<div className="container bottom-0 flex flex-wrap items-center justify-center mx-auto space-y-4 sm:justify-between sm:space-y-0">
 		<div className="flex flex-row pr-3 space-x-4 sm:space-x-8">
 			<div className='p-5 text-2xl text-blue-950 font-extralight dark:text-indigo-200'><Link to='/'>TechFeed</Link></div>
