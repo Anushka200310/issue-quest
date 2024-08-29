29481546ed8689c1f5557c9205a4249ab4995588
Anushka <banerjeeanushka154@gmail.com>
2024-08-29 15:37:37 +0530

    commit

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,4 +1,25 @@
 // Import the functions you need from the SDKs you need
+//import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+//   authDomain: "real-estate-f28eb.firebaseapp.com",
+//   projectId: "real-estate-f28eb",
+//   storageBucket: "real-estate-f28eb.appspot.com",
+//   messagingSenderId: "68586953712",
+//   appId: "1:68586953712:web:1f364aff911104a4171b35"
+// };
+
+// Initialize Firebase
+// export const app = initializeApp(firebaseConfig);
+
+
+
+
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -6,11 +27,11 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "real-estate-f28eb.firebaseapp.com",
-  projectId: "real-estate-f28eb",
-  storageBucket: "real-estate-f28eb.appspot.com",
-  messagingSenderId: "68586953712",
-  appId: "1:68586953712:web:1f364aff911104a4171b35"
+  authDomain: "issue-quest-4fcf7.firebaseapp.com",
+  projectId: "issue-quest-4fcf7",
+  storageBucket: "issue-quest-4fcf7.appspot.com",
+  messagingSenderId: "522231658382",
+  appId: "1:522231658382:web:1f1b3c2b9866137c1656df"
 };
 
 // Initialize Firebase
