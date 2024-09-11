37398367d2ce9fdc35050b981a0cfa61ac1f6c48
Anushka <banerjeeanushka154@gmail.com>
2024-09-11 16:46:58 +0530

    final changes

--- api/server.js
+++ api/server.js
@@ -5,9 +5,12 @@ import errorMiddleware from './middleware/errorMiddleware.js';
 import cors  from "cors";
 import userRouter from './routes/userRouter.js';
 import postRouter from './routes/postRouter.js';
+import path from 'path';
 
 const app = express();
 let PORT = 3000;
+
+const __dirname = path.resolve();
 
 const corsOptions = {
     origin : "http://localhost:5173",
@@ -24,6 +27,12 @@ app.use(express.json());
 app.use("/api/auth", authRouter);
 app.use("/api/user", userRouter);
 app.use("/api/post", postRouter);
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'))
+})
 
 app.use(errorMiddleware);
 
